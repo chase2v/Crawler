169f183048d1aae2f788f45ec2d1699197a1b20b
cheney3w <406653843@qq.com>
2016-12-06 18:36:33 +0800

    update

--- crawler.js
+++ crawler.js
@@ -2,20 +2,67 @@ const http = require('http')
 const fs = require('fs')
 const iconv = require('iconv-lite')
 const cheerio = require('cheerio')
+const url = require('url')
 
-// 缓存
-const cache = []
+// 抓取的新闻缓存
+let cache = []
 
-const server = http.createServer((req, res) => {
-  cache = catchNews()
-
-  res.setHeader({
-    'Content-Type': 'text/plain',
-    'Content-Length': Buffer.byteLength(cache)
-  })
+// 服务器运行逻辑
+http.createServer((req, res) => {
+  // 判断请求方法
+  switch(req.method) {
+    case 'GET':
+      get(req, res)
+      break
+    case 'POST':
+      post(req, res)
+      break
+    case 'PUT':
+      put(req, res)
+      break
+    case 'DELETE':
+    default:
+  }
+}).listen(3333, () => {
+  console.log('正在监听端口3333')
 })
 
-server.listen(3333)
+// 当请求方法为get时，执行此方法
+function get (req, res) {
+  // 判断路径
+  const urlObj = url.parse(req.url)
+  const pathname = urlObj.pathname
+  if (pathname === '/') {
+    getNews(req, res);
+  } else {
+    res.writeHead(404)
+    res.end(404)
+  }
+
+  // 获取数据
+  if (!cache.length) { // 第一次获取
+    cache = catchNews()
+  } else { // 若不是第一次
+    let newData = catchNews()
+    let newSum = filter(newData) // 过滤数据，返回新数据的条数
+    cache.unshift(newData.slice(0, newSum)) // 获取新数据并将其移入缓存的最前面
+  }
+
+  // 渲染页面，返回html页面
+  let page = render(cache)
+
+  // 设置响应头
+  res.setHeader({
+    'Content-Type': 'text/plain',
+    'Content-Length': Buffer.byteLength(page)
+  })
+  res.write()
+
+  // 响应页面
+  res.end(page)
+}
+
+/**********************************以上为主逻辑********************************/
 
 function catchNews () {
   http.get('http://china.nba.com/', (res) => {
@@ -64,4 +111,16 @@ function filter (data) {
       return i
     }
   }
+}
+
+function render (templateUrl, data) {
+  let tpl = fs.readFileSync(templateUrl)
+  const dataList = dataToList(data)
+  const page = tpl.replace('{{content}}', dataList)
+
+  return page
+}
+
+function dataToList (data) {
+  data.forEach()
 }
